cff7a6fae9d7e3b9627a5228f0f99d07124c5300
Jason Raimondi <jason@raimondi.us>
2022-05-05 23:08:34 -0700

    feat: support pdf using jspdf

--- src/blocks.ts
+++
@@ -1,9 +0,0 @@
-export default (editor: any, opts = {}) => {
-  const bm = editor.BlockManager;
-
-  bm.add('MY-BLOCK', {
-    label: 'My block',
-    content: { type: 'MY-COMPONENT' },
-    // media: '<svg>...</svg>',
-  });
-}
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,48 @@
-import loadComponents from './components';
-import loadBlocks from './blocks';
+// @ts-ignore
+import grapesjs from "grapesjs";
+import { jsPDF as HtmlToPdf } from "jspdf";
 
-export default (editor: any, opts = {}) => {
-  const options = { ...{
-    
-    // default options
-  },  ...opts };
+export default (editor: grapesjs.Editor, opts = {}) => {
+  const pfx = editor.getConfig("stylePrefix");
+  const btnPrint = document.createElement("button");
+  const commandName = "gjs-generate-pdf";
 
-  // Add components
-  loadComponents(editor, options);
-  // Add blocks
-  loadBlocks(editor, options);
-  
+  const options = {
+    addPrintButton: true,
+    btnLabel: "Export to PDF",
+    root: (editor: grapesjs.Editor) =>
+      `<!doctype html>
+        <html lang="en">
+          <head>
+            <meta charset="utf-8">
+            <style>${editor.getCss()}</style>
+          </head>
+          <body>${editor.getHtml()}</body>
+        <html>`,
+    ...opts,
+  };
 
-  // TODO Remove
-  editor.on('load', () =>
-    editor.addComponents(
-        `<div style="margin:100px; padding:25px;">
-            Content loaded from the plugin
-        </div>`,
-        { at: 0 }
-    ))
+  btnPrint.innerHTML = options.btnLabel;
+  btnPrint.className = `${pfx}btn-prim`;
+
+  editor.Commands.add(commandName, async (editor: grapesjs.Editor) => {
+    const htmlToPdf = new HtmlToPdf();
+
+    async function savePDF(content: string, js?: string) {
+      if (js) htmlToPdf.addJS(js);
+      await new Promise(resolve => htmlToPdf.html(content, { callback: resolve }));
+      htmlToPdf.save("a4.pdf");
+    }
+
+    await savePDF(options.root(editor), editor.getJs());
+  });
+
+  if (options.addPrintButton) {
+    editor.on("run:generate-pdf", () => {
+      editor.Modal.getContentEl().appendChild(btnPrint);
+      btnPrint.onclick = () => {
+        editor.runCommand(commandName);
+      };
+    });
+  }
 };
